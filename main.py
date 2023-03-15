af7526ac79bb1319fae5d65b035b58de880fca01
brocode <kelimbth@gmail.com>
2023-03-15 08:55:57 +0300

    minor change: fetching API KEY from a file

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import openai
 
+# fetch API KEY from a text file
+with open('API_KEY.txt', 'r') as f:
+    key = f.read()
+
+openai.api_key = f"{key}"
+
+
 messages = []
 
 while True:
     user = input('Enter your question: ')
     if user:
         messages.append({"role": "system", "content": user})
-        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, api_key="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")
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
 
         reply = chat.choices[0].message.content
         print(f'chatGPT: {reply}')
+        print('---'*40)
 
